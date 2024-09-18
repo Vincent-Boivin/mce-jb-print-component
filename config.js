49b2644500c2d382d57564ae3cdcd7259bf428dd
Vincent-Boivin <vboivin78@gmail.com>
2024-09-18 18:27:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -13,85 +13,49 @@
       "description": "Génération d'un fichier de routage"
     }
   },
-  "arguments": {
+"arguments": {
     "execute": {
       "inArguments": [
         {
-          "staticValue": 0,
-          "databoundEmailAddress": "{{Event.EventKey.EmailAddress}}"
+          "emailAddress": "{{InteractionDefaults.Email}}"
+        },
+        {
+          "phoneNumber": "{{Contact.Default.PhoneNumber}}"
         }
       ],
       "outArguments": [
         {
-          "returnValue": "default"
+          "foundSignupDate": ""
         }
       ],
-      "timeout": 10000,
-      "retryCount": 1,
-      "retryDelay": 1000,
-      "concurrentRequests": 6,
-      "url": "https://example.com/execute",
-      "securityOptions": {
-        "securityType": "securityContext",
-        "securityContextKey": "EXTERNAL_KEY"
-      }
+      "url": "https://some-endpoint.com/execute"
     }
   },
   "configurationArguments": {
     "save": {
-      "url": "https://example.com/save",
-      "securityOptions": {
-        "securityType": "securityContext",
-        "securityContextKey": "EXTERNAL_KEY"
-      }
+      "url": "URI/for/your/activity/save"
     },
     "publish": {
-      "url": "https://example.com/publish",
-      "securityOptions": {
-        "securityType": "securityContext",
-        "securityContextKey": "EXTERNAL_KEY"
-      }
+      "url": "URI/for/your/activity/publish"
     },
     "validate": {
-      "url": "https://example.com/validate",
-      "securityOptions": {
-        "securityType": "securityContext",
-        "securityContextKey": "EXTERNAL_KEY"
-      }
+      "url": "URI/for/your/activity/validate"
     },
     "stop": {
-      "url": "https://example.com/stop",
-      "securityOptions": {
-        "securityType": "securityContext",
-        "securityContextKey": "EXTERNAL_KEY"
-      }
+      "url": "URI/for/your/activity/stop"
     }
   },
   "wizardSteps": [
-    {
-      "label": "label1",
-      "key": "step1"
-    },
-    {
-      "label": "label2",
-      "key": "step2"
-    },
-    {
-      "label": "label3",
-      "key": "step3",
-      "active": false
-    }
+    { "label": "Step 1", "key": "step1" },
+    { "label": "Step 2", "key": "step2" },
+    { "label": "Step 3", "key": "step3" },
+    { "label": "Step 4", "key": "step4", "active": false }
   ],
   "userInterfaces": {
     "configModal": {
-      "fullscreen": true,
-      "url": "configModal.html"
-    },
-    "runningModal": {
-      "url": "runningModal.html"
-    },
-    "runningHover": {
-      "url": "runningHover.html"
+      "height": 200,
+      "width": 300,
+      "fullscreen": true
     }
   },
   "schema": {
@@ -99,12 +63,14 @@
       "execute": {
         "inArguments": [
           {
-            "staticValue": {
-              "dataType": "Number",
+            "phoneNumber": {
+              "dataType": "Phone",
               "isNullable": false,
               "direction": "in"
-            },
-            "databoundEmailAddress": {
+            }
+          },
+          {
+            "emailAddress": {
               "dataType": "Email",
               "isNullable": false,
               "direction": "in"
@@ -113,18 +79,14 @@
         ],
         "outArguments": [
           {
-            "returnValue": {
-              "dataType": "Text",
-              "isNullable": false,
-              "direction": "out"
+            "foundSignupDate": {
+              "dataType": "Date",
+              "direction": "out",
+              "access": "visible"
             }
           }
         ]
       }
     }
-  },
-  "copySettings": {
-    "allowCopy": true,
-    "ignoreArguments": ["eventDefinitionKey"]
   }
 }
